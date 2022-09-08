cf4c4add966fbadadee6702fa74e5e98e4cefec8
Guilherme M. Seabra <guiivendrame@users.noreply.github.com>
2022-09-08 14:31:26 -0300

    form and button implemented

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import PageHeader from "../template/pageHeader";
-
+import TodoForm from "./todoForm";
+import Todolist from "./todolist";
 export default class Todo extends Component {
     render() {
         return(
             <div>
                 <PageHeader name="Tarefas" small="Cadastro" />
+                <TodoForm />
+                <Todolist />
             </div>
         )
     }
---
+++ frontend/src/todo/todolist.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default props => (
+    <div>
+        <h1>ToDo List</h1>
+    </div>
+)
