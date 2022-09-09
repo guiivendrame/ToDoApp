84f7f1c08379c141dac125f1f72374044789ef70
Guilherme M. Seabra <guiivendrame@users.noreply.github.com>
2022-09-09 14:43:47 -0300

    post function working

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,22 +1,34 @@
 import React, { Component } from "react";
+import axios from 'axios'
+
 import PageHeader from "../template/pageHeader";
 import TodoForm from "./todoForm";
 import Todolist from "./todolist";
+
+const url = 'http://localhost:3003/api/todos';
 export default class Todo extends Component {
     constructor(props) {
         super(props);
+        this.state = { description: '', list: [] }
+        this.handleChange = this.handleChange.bind(this);
         this.handleAdd = this.handleAdd.bind(this);
     }
     
     handleAdd() {
-        console.log('Add')
+        const description = this.state.description;
+        axios.post(url, { description })
+    }
+
+    handleChange(e) {
+        this.setState({...this.state, description: e.target.value})
     }
 
     render() {
         return(
             <div>
                 <PageHeader name="Tarefas" small="Cadastro" />
-                <TodoForm handleAdd={this.handleAdd}/>
+                <TodoForm description={this.state.description} handleAdd={this.handleAdd}
+                handleChange={this.handleChange} />
                 <Todolist />
             </div>
         )
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -6,9 +6,9 @@ export default props => (
     <div className="todoForm" role="form">
 
         <Grid cols='12 9 10'>
-            <input type="text" id="description" className="form-control" placeholder='Adicione uma tarefa'/>
+            <input type="text" id="description" className="form-control" placeholder='Adicione uma tarefa'
+            value={props.description} onChange={props.handleChange}/>
         </Grid>
-        
 
         <Grid cols='12 3 2'>
             <IconButton style='primary' icon='plus' onClick={props.handleAdd}></IconButton>
